eb98751bf0f6c3cc3a988476e5786083a8e0913a
james yang <jawa.yang@gmail.com>
2022-05-16 11:10:20 +0000

    useState 的使用方式

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react"
+
 import Edit from "./components/Edit";
 import List from "./components/List";
 import "./index.css";
 
 const Home = () => {
+  // 新增一個 useState(預設值) 以取得 [變數,設定變數的函式]
+  const [a,setA] = useState(0)
+  // 設定按鈕點擊(onClick)的行為，呼叫設定變數 a 的函式,且定義該值的變化方式
+  // 原本的值加上 200 後回傳,用來確保每一次的呼叫都是用最後的值去加上 200  
+  const plus = ()=>{
+    setA((prev)=>prev+200)
+  }
+  
   return (
-    <div>
+    <div className="app">
+      <button onClick={plus}>加法</button> &nbsp; 
+      {a}
       <Edit/>
       <List/>
     </div>
