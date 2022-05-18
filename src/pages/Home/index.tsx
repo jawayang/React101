e19023af2804e9449ea5064cbff9d2cb9441ae5f
james yang <jawa.yang@gmail.com>
2022-05-18 11:15:03 +0000

    新增 jsonserver

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,15 @@
 import { useState, useEffect } from "react";
-
 import Edit from "./components/Edit";
 import List from "./components/List";
 import "./index.css";
+
 
 const Home = () => {
   const [data, setData] = useState([]);
 
   useEffect(()=>{
-    //當 data 有變化的時候執行 useEffect 
-    return ()=>{
-    //取消綁定
-    }  
-  },[data])
+
+  },[])
   
   return (
     <div className="app">
